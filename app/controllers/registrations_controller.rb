03b74bb78b13f00ce6e16b7126111d8c2bb75933
James Gilman <jngilman@mac.com>
2016-03-16 10:29:12 -0400

    added regristration controller and redirected devise

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,14 @@
+class RegistrationsController < ApplicationController
+# added the following code based on http://jacopretorius.net/2014/03/adding-custom-fields-to-your-devise-user-model-in-rails-4.html
+ private
+
+  def sign_up_params
+    params.require(:user).permit(:first_name, :middle_initial, :last_name, :email, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:first_name, :middle_initial, :last_name, :email, :password, :password_confirmation, :current_password)
+  end
+end
+
+
